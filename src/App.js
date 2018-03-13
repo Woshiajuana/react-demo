198c77d1a30689cbcc3be84d0d724105d1da645a
Woshiajuana <zhigang.chen@owulia.com>
2018-03-13 16:33:53 +0800

    更新

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react'
 import Head from './components/head'
 import Home from './views/home'
+import List from './views/list'
+import { Route, Router, HashRouter, Switch, Redirect } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-        <div>
-            <Head/>
-            <Home/>
-        </div>
+        <HashRouter>
+            <div>
+                <Head/>
+                <Switch>
+                    {/*<Route exact path='/' component={Home}/>*/}
+                    <Redirect from='/' to='/home'/>
+                    <Route path='/home' component={Home}/>
+                    <Route path='/list' component={List}/>
+                </Switch>
+            </div>
+        </HashRouter>
     );
   }
 }

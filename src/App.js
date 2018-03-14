76671db39e3486349b0f1c6ca5fd0fe1e6bca920
Woshiajuana <zhigang.chen@owulia.com>
2018-03-14 15:07:23 +0800

    新建新的组件

--- src/App.js
+++ src/App.js
@@ -2,22 +2,22 @@ import React, { Component } from 'react'
 import Head from './components/head'
 import Home from './views/home'
 import List from './views/list'
-import { Route, Router, HashRouter, Switch, Redirect } from 'react-router-dom'
+import { Route, Router, BrowserRouter, Switch, Redirect } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-        <HashRouter>
+        <BrowserRouter>
             <div>
                 <Head/>
                 <Switch>
                     {/*<Route exact path='/' component={Home}/>*/}
-                    <Redirect from='/' to='/home'/>
                     <Route path='/home' component={Home}/>
+                    <Redirect exact from='/' to='/home'/>
                     <Route path='/list' component={List}/>
                 </Switch>
             </div>
-        </HashRouter>
+        </BrowserRouter>
     );
   }
 }

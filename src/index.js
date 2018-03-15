5843e127105c662d622fdb9b56824ad5f7d62216
Woshiajuana <zhigang.chen@owulia.com>
2018-03-15 13:56:09 +0800

    目录构建

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom';
-import './assets/scss/_reset.scss'
-import './assets/scss/_define.scss'
-import 'antd/dist/antd.css'
-import App from './App';
+// import Head from './components/head'
+import Home from './views/home'
+import List from './views/list'
+import './App.css'
+import { Route, BrowserRouter, Switch, Redirect } from 'react-router-dom'
+
 import registerServiceWorker from './registerServiceWorker';
+class App extends Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <div>
+                    {/*<Head/>*/}
+                    <Switch>
+                        {/*<Route exact path='/' component={Home}/>*/}
+                        <Route path='/home' component={Home}/>
+                        <Redirect exact from='/' to='/home'/>
+                        <Route path='/list' component={List}/>
+                        {/*<Route path='/404' component={NotFound}/>*/}
+                        {/*<Route path='/admin' component={Admin}/>*/}
+                        {/*<Route component={Front} />*/}
+                    </Switch>
+                </div>
+            </BrowserRouter>
+        );
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();

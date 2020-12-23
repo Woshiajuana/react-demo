8e595dc4b1d5642ddf3f85bc3e2249aec197764d
Woshiajuana <zhigang.chen@owulia.com>
2020-12-23 09:07:18 +0800

    subcode

--- 01helloworld/src/index.js
+++ 01helloworld/src/index.js
@@ -4,14 +4,32 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// react 函数式组件
+function Clock(props) {
+    return (
+        <div>
+            <h1>现在的时间是{props.date.toLocaleTimeString()}</h1>
+            <h2>这是副标题</h2>
+        </div>
+    )
+}
+
+function run() {
+    ReactDOM.render(
+        <Clock date={new Date()}/>,
+        document.getElementById('root'),
+    )
+}
+

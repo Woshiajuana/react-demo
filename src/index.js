fe653a9d158c96add4fe8e390e79da19c76e929f
Woshiajuana <zhigang.chen@owulia.com>
2021-05-10 18:25:19 +0800

    subcode

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter, Route } from 'react-router-dom'
-import Home from 'src/examples/Home'
+import React, { Fragment } from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Route, Switch, Redirect, Link } from 'react-router-dom'
+
+const examples = (s =>
+    s.keys().map(k => ({ key: k.split('/')[1], value: s(k).default }))
+) (require.context('src/examples', true, /index\.js$/));
+
+const Home = () => {
+    return (
+        <ul>
+            { examples.map(({ key }) => <li key={key}><Link to={`/${key}`}>{key}</Link></li>) }
+        </ul>
+    );
+};
 
 const App = () => {
     return (
-        <Route path="/" component={Home}/>
+        <Fragment>
+            <Link to="/">回首页</Link>
+            <Switch>
+                { examples.map(({ key, value }) => <Route key={key} path={`/${key}`} component={value}/>) }
+                <Route path="/" component={Home} exact/>
+                <Redirect to="/" exact/>
+            </Switch>
+        </Fragment>
     );
 };
 

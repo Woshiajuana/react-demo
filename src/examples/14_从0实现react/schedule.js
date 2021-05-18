485d056f6299cdd5932f5417106acb5a097cea91
Woshiajuana <zhigang.chen@owulia.com>
2021-05-18 18:23:31 +0800

    subcode

--- src/examples/14_从0实现react/schedule.js
+++ src/examples/14_从0实现react/schedule.js
@@ -4,10 +4,13 @@
 // diff 阶段
 // render 阶段 此阶段可以暂停
 
+import { TAG_ROOT, ELEMENT_TEXT, TAG_HOST, TAG_TEXT, PLACEMENT, UPDATE, DELETION } from "./constants";
+
 let nextUnitOfWork = null;
 let workInProgressRoot = null; // RootFiber 应用的根
 
 function scheduleRoot(rootFiber) {
+    workInProgressRoot = rootFiber;
     nextUnitOfWork = rootFiber;
 }
 
@@ -25,9 +28,49 @@ function workLoop(deadline) {
     requestIdleCallback(workLoop, { timeout: 500 });
 }
 
-//
-function performUnitOfWork(nextUnitOfWork) {
+// 开始单元工作
+function performUnitOfWork(currentFiber) {
+    beginWork(currentFiber);
+}
 
+// 开始工作
+function beginWork(currentFiber) {
+    if (currentFiber.tag === TAG_ROOT) {
+        updateHostRoot(currentFiber);
+    }
+}
+
+function updateHostRoot(currentFiber) {
+    // [element]
+    // 1. 先处理自己，如果是一个原生节点 创建真实 DOM
+    // 2. 创建子 fiber
+    let newChildren = currentFiber.props.children;
+    reconcileChildren(currentFiber, newChildren);
+}
+
+function reconcileChildren(currentFiber, newChildren) {
+    let newChildIndex = 0; // 新子节点的索引
+    let prevSibling; // 上一个新的子 fiber
+    while (newChildIndex < newChildren.length) {
+        // 取出虚拟 DOM 节点
+        let newChild = newChildren[newChildIndex];
+        let tag;
+        if (newChild.type === ELEMENT_TEXT) {
+            tag = TAG_TEXT; // 这是一个文本
+        } else if (typeof newChild.type === 'string')  {
+            tag = TAG_HOST; // 如果 type 是字符串代表是原生节点
+        }
+        let newFiber = {
+            tag,
+            type: newChild.type,
+            props: newChild.props,
+            stateNode: null,
+            return: currentFiber, // 父Fiber
+            effectTag: PLACEMENT, // 副作用标识
+            nextEffect: null, // effectlist 也是一个单链表
+        };
+        newChildIndex++;
+    }
 }
 
 // react 告诉浏览器  我现在有任务

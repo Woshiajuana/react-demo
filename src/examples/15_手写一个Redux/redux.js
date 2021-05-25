4049caeac48d90b8b1719b5e96e699fe7d6d21df
Woshiajuana <zhigang.chen@owulia.com>
2021-05-25 18:15:13 +0800

    subcode

--- src/examples/15_手写一个Redux/redux.js
+++ src/examples/15_手写一个Redux/redux.js
@@ -12,13 +12,39 @@ const setState = (newState) => {
     listeners.map(fn => fn(state));
 };
 
+
+const prevDispatch = (action) => {
+    setState(reducer(state, action));
+};
+
+let dispatch = (action) => {
+    if (action instanceof Function) {
+        action(dispatch);
+    } else {
+        prevDispatch(action);
+    }
+};
+
+const prevDispatch2 = dispatch;
+
+dispatch = (action) => {
+    if (action.payload instanceof Promise) {
+        action.payload.then(data => {
+            dispatch({...action, payload: data});
+        }).catch(error => {
+            dispatch({...action, payload: error, error: true });
+            return Promise.reject(error);
+        });
+    } else {
+        prevDispatch2(action);
+    }
+};
+
 const store = {
     getState () {
         return state;
     },
-    setState(newState) {
-
-    },
+    dispatch,
     subscribe(fn) {
         listeners.push(fn);
         return () => {
@@ -40,7 +66,6 @@ const changed = (oldState, newState) => {
 
 export const connect = (selector, dispatchSelector) => (Component) => {
     return (props) => {
-        const {setState} = useContext(appContext);
         const dispatch = (action) => {
             setState(reducer(state, action));
         };
